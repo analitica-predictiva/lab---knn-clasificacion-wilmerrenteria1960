2a94a3d22be58c80e3de0a8c29d93b0056970023
Wilmer Renteria cespedes <wilmer@MacBook-Pro-de-Wilmer.local>
2022-12-02 18:14:26 -0500

    update

--- preguntas.py
+++ preguntas.py
@@ -66,7 +66,7 @@ def pregunta_02():
     
 
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6)
+    knn = KNeighborsClassifier(n_neighbors=5)
 
     # Entrene el clasificador con el conjunto de entrenamiento
     knn.fit(X, y)
